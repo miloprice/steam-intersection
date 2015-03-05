a1d9b8086beb764d2833ada96b825509cabdeb4c
miloprice <milo.price@gmail.com>
2015-03-04 17:51:23 -0800

    formatting, simple error handling in case of serverside error

--- steam_intersection.py
+++ steam_intersection.py
@@ -8,7 +8,7 @@ parser.add_argument('--steamids', type=str, metavar = "ID", nargs=2, help='steam
 args = parser.parse_args()
 
 params1 = dict(
-	key = args.key,
+    key = args.key,
     steamid = args.steamids[0],
     include_appinfo = 1,
     include_played_free_games = 0,
@@ -16,20 +16,23 @@ params1 = dict(
 )
 
 params2 = dict(
-	key = args.key,
-	steamid = args.steamids[1],
-	include_appinfo = 1,
-	include_played_free_games = 0,
-	format="json"
+    key = args.key,
+    steamid = args.steamids[1],
+    include_appinfo = 1,
+    include_played_free_games = 0,
+    format="json"
 )
 
-resp1 = requests.get(url=url, params=params1)
-resp2 = requests.get(url=url, params=params2)
-data1 = json.loads(resp1.text)
-data2 = json.loads(resp2.text)
-games1 = [x['name'] for x in data1['response']['games']]
-games2 = [x['name'] for x in data2['response']['games']]
-common_games = set(games1).intersection(set(games2))
-print("Games in common:")
-for game_title in common_games:
-	print game_title
+try:
+    resp1 = requests.get(url=url, params=params1)
+    resp2 = requests.get(url=url, params=params2)
+    data1 = json.loads(resp1.text)
+    data2 = json.loads(resp2.text)
+    games1 = [x['name'] for x in data1['response']['games']]
+    games2 = [x['name'] for x in data2['response']['games']]
+    common_games = set(games1).intersection(set(games2))
+    print("Games in common:")
+    for game_title in common_games:
+        print game_title
+except ValueError:
+    print("Error: Unexpected response content")
